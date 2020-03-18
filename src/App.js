2af441cd006c4d6418fadc3d439e17882356efa3
buckleader <jasmin28virdi@gmail.com>
2020-03-18 22:42:05 +0530

    added dummy js file

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render } from "react-dom";
+
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "ChatBot Compiler"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havana"
+    })
+  ]);
+};
+
+render(React.createElement(App), document.getElementById("root"));
