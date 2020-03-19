af1216964b9a8be7a3d39746562897d7109a05c1
buckleader <jasmin28virdi@gmail.com>
2020-03-20 00:51:33 +0530

    webpack.config.js file

---
+++ src/components/views/CodeEditor/CodeEditor.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class CodeEditor extends Component {
+  render() {
+    return (
+      <div
+        id="container"
+        style="width: 800px; height:600px; border: 1px solid #ccc;"
+      ></div>
+    );
+  }
+}
