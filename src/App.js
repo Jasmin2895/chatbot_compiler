01a769c2969c872279d464d311b460c0b24f14a2
buckleader <jasmin28virdi@gmail.com>
2020-03-20 16:54:25 +0530

    updated package.json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { Component } from "React";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+  }
   render() {
-    return <p>This is my new react App version 1.2</p>;
+    return <p>This is my new react App version 1.1</p>;
   }
 }
 
