740cb7e4486a8c8d5542696250addf89551e03c2
buckleader <jasmin28virdi@gmail.com>
2020-03-20 22:47:51 +0530

    app created with react app

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
-import MonacoEditor from "react-monaco-editor";
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      code: "//tyep your code here"
-    };
-  }
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-  render() {
-    const code = this.state.code;
-    const options = {
-      selectOnLineNumbers: true
-    };
-
-    editorDidMount = (editor, moncao) => {
-      console.log("editorDidMount", editor);
-      editor.focus();
-    };
-
-    onChange = () => {
-      console.log("onChange", newValue, e);
-    };
-    return (
-      <div>
-        <MonacoEditor
-          width="800"
-          height="600"
-          language="javascript"
-          theme="vs-dark"
-          value={code}
-          options={options}
-          onChange={this.onChange}
-          editorDidMount={this.editorDidMount}
-        />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-render(<App />, document.getElementById("root"));
+export default App;
