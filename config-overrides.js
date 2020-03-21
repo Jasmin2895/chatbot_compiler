534c16d8cd869647cb6062ebb16f1a4ec8602382
buckleader <jasmin28virdi@gmail.com>
2020-03-21 17:58:02 +0530

    react monaco editor running

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,11 @@
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
 module.exports = function override(config, env) {
-  config.resolve = {
-    alias: {
-      "monaco-editor": "monaco-editor/esm/vs/editor/editor.api.js"
-    }
-  };
+  config.resolve = {};
   config.plugins.push(
     new MonacoWebpackPlugin({
-      languages: ["json"]
+      features: ["!gotoSymbol"],
+      languages: ["javascript", "typescript", "python", "java", "ruby"]
     })
   );
   return config;
